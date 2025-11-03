8aa1e7995b62b1ee5aa00eb517ed3cbac6115fa9
thomasIemaire <thomas.lmre14@gmail.com>
2025-11-03 18:57:19 +0100

    ajustements

--- src/app/components/choice/choice.component.ts
+++ src/app/components/choice/choice.component.ts
@@ -12,19 +12,16 @@ import { QuestionChoice } from "../../core/models/question.model";
         [style.animationDelay]="(index * 0.1) + 's'">
         <ng-container [ngSwitch]="choice.input">
             <div *ngSwitchCase="'text'" class="choice__input-wrapper">
-                <label class="choice__text choice__text--editable">
-                    <span class="choice__label">{{ choice.text }}</span>
-                    <input
-                        class="choice__input"
-                        type="text"
-                        [value]="value"
-                        [attr.placeholder]="choice.text"
-                        [attr.minlength]="choice.constraints?.minLength ?? null"
-                        [attr.maxlength]="choice.constraints?.maxLength ?? null"
-                        (input)="onValueInput($event)"
-                        [disabled]="disabled"
-                    />
-                </label>
+                <input
+                    class="choice__input"
+                    type="text"
+                    [value]="value"
+                    [attr.placeholder]="choice.text"
+                    [attr.minlength]="choice.constraints?.minLength ?? null"
+                    [attr.maxlength]="choice.constraints?.maxLength ?? null"
+                    (input)="onValueInput($event)"
+                    [disabled]="disabled"
+                />
             </div>
 
             <div *ngSwitchCase="'number'"
